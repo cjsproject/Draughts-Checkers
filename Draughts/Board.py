8d27642b638706727d1338e2eb2d171c9d6c970d
divin <cyrus.simons@yahoo.com>
2021-06-25 23:12:44 -0400

    implemented move and board update

--- Draughts/Board.py
+++ Draughts/Board.py
@@ -14,6 +14,7 @@ todo:
 BOARD_SIZE = 8
 
 def tile_num_to_index(tile_num):
+	# returns the board's index of the green tile with labeled tile_num
 	evens = [0, 2, 4, 6]
 	odds = [1, 3, 5, 7]
 
@@ -25,6 +26,16 @@ def tile_num_to_index(tile_num):
 
 	return row, col
 
+def parse_move(move_string):
+	# parses a move of the for '12-16' or '21x14' to indicate move or capture
+	if 'x' in move_string:
+		jump = True
+		tile1, tile2 = move_string.split('x')
+	elif '-' in move_string:
+		jump = False
+		tile1, tile2 = move_string.split('-')
+	
+	return int(tile1), int(tile2), jump
 
 
 class Player:
@@ -32,15 +43,24 @@ class Player:
 		self.player_color = player_color
 
 	def select_tile(self, tile_num, board):
+		# converts the tile number input to an index on mainboard
 		i, j = tile_num_to_index(tile_num)
-		if not board.Board[i][j]._valid_tile(self.player_color):
+		# validates the tile has a piece which belongs to the player
+		if not board.Board[i][j].validate_tile(self.player_color):
 			return
 		print(board.Board[i][j])
 
-	def move(self, pce_on_tile, destination):
-		if pce_on_tile._valid_piece(self.player_color):
-			return 
-		return
+	def move(self, move_string):
+		# Parses move, returns selected tile and the next tile to move to
+		current_tile, nxt_tile, jump = parse_move(move_string)
+		# checks to
+		#self.select_tile(current_tile, mainboard)
+		print()
+		i, j = tile_num_to_index(current_tile)
+		i1, j1 = tile_num_to_index(nxt_tile)
+		mainboard.Board[i1][j1].piece = mainboard.Board[i][j].piece
+		mainboard.Board[i][j].piece = False
+		print(mainboard)
 
 
 class Board:
@@ -50,8 +70,9 @@ class Board:
 
 	def _createBoard(self):
 		# setting up board
-		# each Tile() holds tile information (color of tile, Piece() object)
-		# Piece() objects have color info r/w 
+		# each Tile() holds tile information (color of tile, Piece() object), 
+		# tile no. updated for green tiles
+		# Piece() objects have color info: r/w and king info
 		self.Board = [[Tile((i+(j%2))%2, Piece('w')) if (j < 3 and (i+(j%2))%2) or (j > 4 and (i+(j%2))%2) else Tile((i+(j%2))%2, False) for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
 
 		Board = self.Board
@@ -79,17 +100,28 @@ class Tile:
 		string = f"({str(self.number)}, {self.piece})"
 		return string
 
-	# used to compare tiles in can_move?
 	def __eq__(self, other):
+		# used to compare tiles in can_move?
 		return self.color == other.color and self.number == other.number
 
-	def _valid_tile(self, player_color):
-		piece_available = self.piece
-		same_color = self.piece.color == player_color
-		movable = self.piece.can_move()
-		if piece_available and same_color and movable:
+	def tile_is_empty(self):
+		return Bool(self.piece)
+
+	def validate_tile(self, player_color):
+		# validates the tile contains a piece which belongs to the player
+		#piece_available = self.piece
+		piece_on_tile = bool(self.piece)
+		if piece_on_tile:
+			player_owns_piece = (self.piece.color == player_color)
+
+		#movable = self.piece.can_move() or not self.piece
+		#if piece_available and same_color and movable:
+		#	return True
+		#return False
+		if self.number and piece_on_tile:
 			return True
 		return False
+
 
 
 
@@ -119,6 +151,8 @@ print(mainboard)
 
 print()
  
-player = Player('w')
-player.select_tile(5, mainboard) # does not print anything, piece doesn't belong to player color
-player.select_tile(32, mainboard) # prints the value at board index [2, 3] which contains a red piece at tile num 10
+player = Player('r')
+#player.select_tile(5, mainboard) # does not print anything, piece doesn't belong to player color
+#player.select_tile(32, mainboard) # prints the value at board index [2, 3] which contains a red piece at tile num 10
+
+player.move('11-15')
